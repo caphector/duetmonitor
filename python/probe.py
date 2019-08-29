d546462526b2bede82a51a7af8aec3ad809d9966
Duncan <me@caphector.com>
2019-08-28 19:48:22 -0700

    Updated probe

--- python/probe.py
+++ python/probe.py
@@ -8,6 +8,7 @@ import time
 from os.path import expanduser
 # import pysnooper
 from timeit import default_timer as timer
+import logging
 
 ip = '192.168.1.88'
 baseurl = 'http://' + ip + '/'
@@ -25,10 +26,12 @@ autocalibration = 'G32'
 logger = logging.getLogger('duet-log')
 
 
-
 def scan_result(gcode):
     result = du.wait_until_ready(du.send_gcode(gcode))
     return result
+
+
+logger = logging.getLogger('duet-log')
 
 
 # @pysnooper.snoop()
@@ -36,27 +39,26 @@ def main():
     probe_dev = 2
     ready = 0.030
     du.send_gcode(du.gcoder('home'))
-    du.warmup('pla')
+#    du.warmup('pla')
 #    time.sleep(300) # Wait for it to warm up
     i = 1
     log = 'Doing large radius calibration'
     du.log_and_print(log.format(i), 'initial_calibration')
     t1 = timer()
     while probe_dev > ready:
-        du.log_and_print('Doing small radius calibration #{}'.format(i), 'secondary_calibration')
+        logger.info('Doing small radius calibration #{}'.format(i))
         start = timer()
         result = scan_result(regularprobe)
         end = timer()
         timed = (end - start)
-        du.log_and_print('Probing took {} seconds'.format(timed), 'timer')
         cal, probe_mean, probe_dev = du.probe_parse(result)
-        du.log_and_print('Completed calibration #{}. Mean: {} Dev: {}'.format(i, probe_mean, probe_dev), 'secondary_calibration')
+        logger.info('Completed calibration #{}. Mean: {} Dev: {} in {}'.format(i, probe_mean, probe_dev, timed))
         time.sleep(30)
     du.log_and_print('Calibration is under {} mean deviation after {} calibrations; ready to print after autocalibration.'.format(probe_dev, i), 'done_calibrating')
     du.send_gcode(du.gcoder('autocal'))
     t2 = timer
     calibration_time = (t2 - t1)
-    du.log_and_print('Calibration took {} seconds'.format(calibration_time), 'timer')
+    logging.info('Calibration took {} seconds'.format(calibration_time), 'timer')
 
 
 main()
